502c250dfbb89db27c250dc273fd280ddf3b05da
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-13 16:36:00 +0000

    script of variable and their tpes

---
+++ 01_variable/variable_types.js
@@ -0,0 +1,17 @@
+function variableTypes(){
+    var num = 6
+    console.log(num)
+    console.log(typeof(num))
+    let sathya = "sathya"
+    console.log(sathya)
+    console.log(typeof(sathya))
+    const isGood =true
+    console.log(isGood)
+    console.log(typeof(isGood))
+    
+    const isObj ={ "name":"prakash","year":1992}
+    console.log(isObj)
+    console.log(typeof(isObj))
+}
+
+variableTypes();
