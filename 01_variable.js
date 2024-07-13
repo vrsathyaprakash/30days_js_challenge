a9a2465febdcefea017d100b72f935a13271b562
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-13 16:05:59 +0000

    variables and types

--- 01_variable.js
+++ 01_variable.js
@@ -1,1 +1,16 @@
-let sathya = 55
+var num = 6
+console.log(num)
+console.log(typeof(num))
+let sathya = "sathya"
+console.log(sathya)
+console.log(typeof(sathya))
+const isGood =true
+console.log(isGood)
+console.log(typeof(isGood))
+
+const isObj ={ "name":"prakash","year":1992}
+console.log(isObj)
+console.log(typeof(isObj))
+
+//isGood = false
+//console.log(isGood)
