0231031e096779c1c7020e3088d16303ce71c8fd
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-13 17:06:02 +0000

    variables with const and let key word

---
+++ 01_variable/const_let.js
@@ -0,0 +1,9 @@
+function constLet(){
+    const sathya= "sathya"
+    let prakash = "prakash"
+
+    prakash=6
+    console.log('variable defined with let key word is mutable where as variable defined with const key word is immutable')
+}
+
+constLet()
