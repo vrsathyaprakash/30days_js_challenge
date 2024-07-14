bfbf064b1c1f6712d36f7d85a01365da452ae68e
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-14 04:07:12 +0000

    basic operations

---
+++ 02_operators/operators.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    console.log(a+b)
+}
+
+function sub(a,b){
+    console.log(a-b)
+}
+
+function mul(a,b){
+    console.log(a*b)
+}
+
+function div(a,b){
+    console.log(a/b)
+}
+
+add(4,5)
+sub(4,5)
+mul(4,5)
+div(4,8)
