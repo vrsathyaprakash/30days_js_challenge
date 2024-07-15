d46993c9f2ef11371ee133af6770535ecbd791dd
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-15 05:39:03 +0000

    day of week

---
+++ 03_control_structures.js/04_switch_case_day_of week.js
@@ -0,0 +1,31 @@
+function dayOfWeeek(day){
+    switch(day){
+        case 1:
+            console.log("Monday")
+            break;
+        case 2:
+            console.log("Tuesday")
+            break;
+        case 3:
+            console.log("Wednesday")
+            break;
+        case 4:
+            console.log("Thursday")
+            break;
+        case 5:
+            console.log("Friday")
+            break;
+        case 6:
+            console.log("saturday")
+            break;
+        case 7:
+            console.log("sunday")
+            break;
+        
+    }
+}
+
+
+dayOfWeeek(7)
+dayOfWeeek(4)
+dayOfWeeek(3)
