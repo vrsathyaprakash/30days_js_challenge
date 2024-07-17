6af21e37a2746a8b22deb76810bd1b00b761b75c
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-17 03:29:00 +0000

    file name chnge

---
+++ 03_control_structures/01_number.js
@@ -0,0 +1,13 @@
+function number(a){
+    if(a>0){
+     console.log("a is a positive number")
+    }else if(a<0){
+        console.log("a is a negative number")
+    }else{
+        console.log("a is a zero")
+    }
+}
+
+number(5)
+number(0)
+number(-5)
---
+++ 03_control_structures/06_evenOdd.js
@@ -0,0 +1,7 @@
+function evenOdd(a){
+    a = (a%2===0) ? "even" : "odd" 
+    console.log(a)
+}
+
+evenOdd(3)
+evenOdd(56)
