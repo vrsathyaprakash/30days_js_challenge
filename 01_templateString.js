4fbc3883f1f5a5a40d6a178f1355eda115e428f0
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-20 04:33:57 +0000

    Tempelate literals

---
+++ 01_templateString.js
@@ -0,0 +1,15 @@
+const person={
+    name:"Sathya Prakash",
+    age:32
+}
+
+const aboutPerson=`I am ${person.name} and my age is ${person.age} years`
+
+console.log(aboutPerson)
+
+const multilineTemplateString=`The university was established as "University of Health Sciences" by the Government of 
+Andhra Pradesh and was inaugurated on 9 April by N. T. Rama Rao, the then-Chief Minister of the state. It started functioning
+ on 1 November 1986[2] with Rama Rao as the university's first chancellor. Following the death of Rama Rao, a directive was 
+ issued by the Government of Andhra Pradesh to rename the university "Dr. NTR University of Health Sciences", carried out on
+  2 February 1998.[3] The university celebrated its silver jubilee from 1–3 November 2011.`
+  console.log(multilineTemplateString)
