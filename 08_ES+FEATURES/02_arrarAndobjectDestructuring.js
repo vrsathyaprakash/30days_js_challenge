2b41b6f981d8ae59a542642c7d84035e52acd91e
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-20 05:13:32 +0000

    object destructuring

--- 08_ES+FEATURES/02_arrarAndobjectDestructuring.js
+++ 08_ES+FEATURES/02_arrarAndobjectDestructuring.js
@@ -3,3 +3,13 @@ const cars = ["Saab", "Volvo", "BMW"];
 const [cars1, cars2, cars3]=cars;
 
 console.log(cars1, cars2, cars3)
+
+
+let person = {
+    firstName: 'John',
+    lastName: 'Doe'
+};
+
+let {firstName,lastName}=person;
+console.log(firstName)
+console.log(lastName)
