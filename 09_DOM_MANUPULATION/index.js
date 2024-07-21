00a59e4644554f97dae8d25ea863fad2619217fe
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-21 07:37:33 +0000

    new

--- 09_DOM_MANUPULATION/index.js
+++ 09_DOM_MANUPULATION/index.js
@@ -3,9 +3,13 @@ document.getElementById('title').innerText="Java script challemge DAY 9"
 document.querySelector(".content").innerText= `Challenge about dom manipulation includes changing content
  by selecting id and class inside html`
 
- document.querySelector('.content').style.backgroundColor="#C0C0C0"
+ document.querySelectorAll('.content').style.backgroundColor="#C0C0C0"
 
+ //ceate a new div element
  const newdev = document.createElement('div')
  const text=document.createTextNode("new div elemnt inside  a body element")
  newdev.appendChild(text)
  document.body.append(newdev)
+
+
+ //create a new li element and aapend  it to the existing ui element
