fa995bcb741e8303279fe607b83aba9cbcdb9e99
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-24 15:39:11 +0000

    change image atribute add and remove class for an element

--- 09_DOM_MANUPULATION/index.js
+++ 09_DOM_MANUPULATION/index.js
@@ -3,7 +3,7 @@ document.getElementById('title').innerText="Java script challemge DAY 9"
 document.querySelector(".content").innerText= `Challenge about dom manipulation includes changing content
  by selecting id and class inside html`
 
- document.querySelectorAll('.content').style.backgroundColor="#C0C0C0"
+ document.querySelector('.content').style.backgroundColor="#C0C0C0"
 
  //ceate a new div element
  const newdev = document.createElement('div')
@@ -13,3 +13,24 @@ document.querySelector(".content").innerText= `Challenge about dom manipulation 
 
 
  //create a new li element and aapend  it to the existing ui element
+ const newLi = document.createElement('li')
+ const context = document.createTextNode("REACT")
+ newLi.appendChild(context)
+ document.getElementById('lists').appendChild(newLi)
+
+
+ //to deleate a element from document
+const reELe = document.getElementById('con')
+reELe.remove()
+
+//to remove last child of html-ul elemnt
+const list=document.getElementById('lists')
+list.lastElementChild.remove()
+
+//to change the attribute of an html element
+const image=document.getElementById('react-icon')
+image.src= "react.png"
+
+//to add and remove class element
+list.classList.add('bkg')
+list.classList.remove('bkg')
