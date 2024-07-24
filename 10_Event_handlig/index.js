d6ed1fcd703d4c15e7720501e5c235ffb99ba37e
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-24 19:28:13 +0000

    key down

---
+++ 10_Event_handlig/index.js
@@ -0,0 +1,34 @@
+const chBtn=document.getElementById("change")
+const chPara=document.getElementById("change-para")
+const chInput = document.getElementById("keyinput")
+const chEvent = document.getElementById("keyevent")
+
+
+chBtn.addEventListener('click',()=>{
+    chPara.innerText=`The tax rates have been adjusted. Previously, there was no tax on income up to ₹3 lakhs, 
+    5% on income between ₹3 lakhs and ₹6 lakhs, 10% on income between ₹6 lakhs and ₹9 lakhs and 15% on income 
+    between ₹9 lakhs and ₹12 lakhs. Now, the 5% rate applies to income between ₹3 lakhs and ₹7 lakhs, the 10% 
+    rate covers ₹7 lakhs to ₹10 lakhs and the 15% rate starts at ₹10 lakhs and goes up to ₹12 lakhs. The rest 
+    remains the same.`
+})
+
+chBtn.addEventListener('dblclick',()=>{
+       const reImg = document.getElementById("react-img")
+       if(reImg.style.display=="none"){
+          reImg.style.display='block'
+       }else{
+          reImg.style.display = 'none'
+       }
+})
+
+chBtn.addEventListener('mouseover',()=>{
+   chPara.style.backgroundColor='red'
+})
+
+chBtn.addEventListener('mouseout',()=>{
+   chPara.style.backgroundColor='#ffffff'
+})
+
+chInput.addEventListener('keydown',(e)=>{
+   chEvent.innerText +=`${e.code}`
+})
